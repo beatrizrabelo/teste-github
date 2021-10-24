94bb8696a3a58749e39fecbe486c08481f868c7b
Beatriz Rabelo <beatriz.devrabelo@gmail.com>
2021-10-24 15:21:55 -0300

    Acrescentada Mensagem Teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,6 @@ public class Program {
     public static void main (String [] args){
         
         System.out.println("Testing");
-        
+        System.out.println("Testing too");
     }
 }
